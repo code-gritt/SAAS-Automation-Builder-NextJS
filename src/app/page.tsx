2f0fe44fbbcb617cb0c7fac686b021568c7e865f
gokul <gokulchandan24@gmail.com>
2024-10-01 00:04:40 +0530

    app started

---
+++ src/app/page.tsx
@@ -0,0 +1,131 @@
+import { HeroParallax } from "@/components/global/connect-parallax";
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
+import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
+import Navbar from "@/components/global/navbar";
+import { Button } from "@/components/ui/button";
+
+export const clients = [...new Array(10)].map((client, index) => ({
+  href: `/${index + 1}.png`,
+}));
+
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail: "/p3.png",
+  },
+
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail: "/p4.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail: "/p5.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail: "/p6.png",
+  },
+
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail: "/p3.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail: "/p4.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail: "/p5.png",
+  },
+
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail: "/p6.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail: "/p3.png",
+  },
+];
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex bg-neutral-950 text-white items-center justify-center flex-col">
+        <Navbar />
+        <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+          <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+          <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+            <ContainerScroll
+              titleComponent={
+                <div className="flex items-center flex-col">
+                  <Button
+                    size={"lg"}
+                    className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                  >
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                      Start For Free Today
+                    </span>
+                  </Button>
+                  <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                    Automate Your Work With Ares
+                  </h1>
+                </div>
+              }
+            />
+          </div>
+        </section>
+        <InfiniteMovingCards
+          className="md:mt-[18rem] mt-[-100px]"
+          items={clients}
+          direction="right"
+          speed="slow"
+        />
+        <section>
+          <HeroParallax products={products}></HeroParallax>
+        </section>
+      </main>
+    </>
+  );
+}
